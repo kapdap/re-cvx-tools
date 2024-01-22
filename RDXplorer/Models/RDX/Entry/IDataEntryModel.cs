7a3f1a651cb2acc76a9dcc66d391d735c49161ab
Kapdap <kapdap@pm.me>
2024-01-23 01:48:48 +1300

    Add IsPointer and IsText to DataEntryModel interface

--- RDXplorer/Models/RDX/Entry/IDataEntryModel.cs
+++ RDXplorer/Models/RDX/Entry/IDataEntryModel.cs
@@ -8,5 +8,8 @@ namespace RDXplorer.Models.RDX
         public long Size { get; }
         public byte[] Data { get; }
         public string Text { get; }
+
+        public bool IsPointer { get; set; }
+        public bool IsText { get; set; }
     }
 }
