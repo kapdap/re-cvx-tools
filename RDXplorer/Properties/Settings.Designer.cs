4b212305ec471cea017ade223a2887d01241c314
Kapdap <kapdap@pm.me>
2024-01-26 20:01:07 +1300

    Add tmp_path setting

--- RDXplorer/Properties/Settings.Designer.cs
+++ RDXplorer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RDXplorer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,18 @@ namespace RDXplorer.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("tmp_dir")]
+        public string tmp_path {
+            get {
+                return ((string)(this["tmp_path"]));
+            }
+            set {
+                this["tmp_path"] = value;
             }
         }
     }
