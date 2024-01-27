460d2d5d09d24ba9ae37ce235c256fb684a73cc7
Kapdap <kapdap@pm.me>
2024-01-27 18:30:49 +1300

    Add DataModel base class and interface

---
+++ RDXplorer/Models/RDX/Data/DataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RDXplorer.Models.RDX
+{
+    public class DataModel<T> : IDataModel<T>
+        where T : IFieldsModel, new()
+    {
+        public IntPtr Position { get; set; }
+        public uint Size { get; set; }
+        public T Fields { get; private set; } = new();
+    }
+}
---
+++ RDXplorer/Models/RDX/Data/IDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RDXplorer.Models.RDX
+{
+    public interface IDataModel<T>
+        where T : IFieldsModel, new()
+    {
+        IntPtr Position { get; set; }
+        uint Size { get; set; }
+        T Fields { get; }
+    }
+
+    public interface IFieldsModel
+    {
+    }
+}
