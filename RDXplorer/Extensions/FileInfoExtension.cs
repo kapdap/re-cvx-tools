dd6f5a5c3e5c7c1ec493caca5627306815b98d42
Kapdap <kapdap@pm.me>
2024-01-29 21:24:20 +1300

    Add OpenReadShared FileInfo extension

---
+++ RDXplorer/Extensions/FileInfoExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace RDXplorer.Extensions
+{
+    public static class FileInfoExtension
+    {
+        public static FileStream OpenReadShared(this FileInfo file) =>
+            file.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+    }
+}
